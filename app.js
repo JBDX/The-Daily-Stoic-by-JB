03a739e2e5561d1b9acdf99809822c152d0490f9
JBDX <jbdx.dev@gmail.com>
2020-04-25 10:23:06 -0400

    Adds Node module

---
+++ app.js
@@ -0,0 +1,20 @@
+//jshint esversion: 6
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const https= require("https")
+
+const app = express()
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res){
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(3000, function(){
+    console.log("Server is up and running baby!");
+})
+
