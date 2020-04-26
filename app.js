9fc91e47390cdffdc7b06f4eb95c9c18cab64bb2
JBDX <jbdx.dev@gmail.com>
2020-04-25 21:55:26 -0400

    Adds Partials plus base pages.

--- app.js
+++ app.js
@@ -7,12 +7,32 @@ const https= require("https")
 
 const app = express()
 
+app.set('view engine', 'ejs');
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function (req, res){
-    res.sendFile(__dirname + "/public/index.html");
+app.get("/home", function (req, res){
+    res.render("home");
 });
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+app.get("/sources", function(req, res){
+    res.render("sources");
+});
+app.get("/copyright", function(req, res){
+    res.render("copyright");
+});
+app.get("/language", function(req, res){
+    res.render("language");
+});
+app.get("/subscribe", function(req, res){
+    res.render("subscribe");
+});
+
+
 
 app.listen(3000, function(){
     console.log("Server is up and running baby!");
